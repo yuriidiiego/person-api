e43ae0eabd0bcbb51d6c634ac044b49aef6fa75e
Yuri Nascimento <yuriidiiego@gmail.com>
2023-01-16 18:28:33 -0300

    changing from people to person

--- src/main/java/com/attornatus/project/PersonApplication.java
+++ src/main/java/com/attornatus/project/PersonApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeopleApplication {
+public class PersonApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(PeopleApplication.class, args);
+    SpringApplication.run(PersonApplication.class, args);
   }
 }
